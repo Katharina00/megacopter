e1b1b8ff131b061114fe6d1105fec5f1f8ab6da9
Katharina00 <khenrich0205@hotmail.de>
2023-01-30 14:05:25 +0100

    update

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Aboutpage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>This is us.</h2>
+        </div>
+    )
+}
+
+export default Aboutpage
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blogpage = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <h2>Posts will go here.</h2>
+        </div>
+    )
+}
+
+export default Blogpage
